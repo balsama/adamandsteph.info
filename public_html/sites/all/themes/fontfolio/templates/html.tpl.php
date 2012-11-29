693e5c5cfb44d9a8a098a20af8ccd8fce6e37f77
adam balsam <adam.balsam@brunellocreative.com>
2012-11-29 09:43:15 -0500

    Added GA code

--- public_html/sites/all/themes/fontfolio/templates/html.tpl.php
+++ public_html/sites/all/themes/fontfolio/templates/html.tpl.php
@@ -62,6 +62,20 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  
+  <!-- GA Code -->
+  <script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36675861-1']);
+  _gaq.push(['_setDomainName', 'adamandsteph.info']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+  </script>
+  <!-- /EOF GA Code -->
 
 </body>
 </html>
