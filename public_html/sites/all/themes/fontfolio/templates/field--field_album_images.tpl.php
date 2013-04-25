bd0ec92ed14dc48a5b00bd30c5a5bc9b2c63b15c
Adam Balsam <balsam.adam@gmail.com>
2013-04-25 19:36:59 -0400

    lightbox on gallery nodes

--- public_html/sites/all/themes/fontfolio/templates/field--field_album_images.tpl.php
+++ public_html/sites/all/themes/fontfolio/templates/field--field_album_images.tpl.php
@@ -11,11 +11,17 @@
   // Make $items to contain only 1 item;
   $items = array_slice($items, 0, 1);
  }
+
+drupal_add_js(base_path() . drupal_get_path('theme', 'fontfolio') . '/libraries/fancybox/jquery.fancybox-1.3.4.js');
 ?>
+  <pre><?php //print_r($foo); ?></pre>
+
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
-      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
+      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
+        <?php print l(render($item), file_create_url($item['#item']['uri']), array('html' => TRUE, 'attributes' => array('class' => 'box', 'rel' => 'gallery'))); ?>
+      </div>
       <div class="photo-options">
         <a class="plusoneapi-votes" id="plusoneapi-votes-<?php print $item['#item']['fid']; ?>" rel="<?php print $item['#item']['fid']; ?>" title="Plus 1">
           <span class="heart">Votes:</span>
@@ -37,3 +43,14 @@
 </div>
 <?php drupal_add_js(drupal_get_path('module', 'plusoneapi') . '/plusoneapi.js'); ?>
 
+<script>
+jQuery(document).ready(function() {
+  jQuery('.box').fancybox({
+    'type': 'image',
+      'overlayColor': '#000',
+      'overlayOpacity': '0.7',
+      'padding': 0,
+      'margin': 13,
+  });
+});
+</script>
